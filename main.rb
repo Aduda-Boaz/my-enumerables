b9eff9e99e6e81c364efc0e089879c1406edd8fe
Aduda-Boaz <adudaboaz@gmail.com>
2021-04-23 00:10:55 +0300

    Add my-select enumarable method

--- main.rb
+++ main.rb
@@ -29,16 +29,42 @@ module Enumerable
     ticket
   end
 
+  #my_select
+  def my_select
+    return to_enum(:my_each) unless block_given?
+    
+    choose = []
+    my_each { |item| choose.push(item) if yield(item) } 
+    choose
+  end
 
+  #my_all
+
+  def my_all(arr)
+    belong = true
+
+    #check it the items belong to the same class
+    if !arr[0].nil?
+      my_each { |item| belong = false unless arr[0] === item }
+    elsif !block_given?
+      my_each { |item| belong = false unless item }
+    else 
+      my_each { |item| belong = false unless yield(item) }
+    end
+    belong
+  end
 
 end
 
 # Tests
 
 puts '#my_each'
-%w[Jenifar, Aaron, Mike, Tom, Chantelle].my_each { |friends| puts friends }
+%w[Jenifar, Aaron, Mike, Tom, Chantelle].my_each { |name| puts name }
 
 puts '#my_each_with_index'
-%w[Jenifar, Aaron, Mike, Tom, Chantelle].my_each_with_index { |friends, index| puts friends if index.even? }
+%w[Jenifar, Aaron, Mike, Tom, Chantelle].my_each_with_index { |name, index| puts name if index.odd? }
+
+puts '#my_select'
+puts (%w[Jenifar, Aaron, Mike, Tom, Chantelle].my_select { |name|  name != 'Aaron' })
 
 
